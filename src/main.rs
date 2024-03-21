06eb43c58f6f7c1b633f6230a725e49d2bb962ff
Zaki Machfj <zmachfj@uwaterloo.ca>
2024-03-20 22:29:00 -0400

    respond to multiple PINGS

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::io::prelude::*;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => stream
-                .write_all("+PONG\r\n".as_bytes())
-                .expect("Failed to PONG"),
+            Ok(stream) => {
+                handle_connection(stream);
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    loop {
+        stream
+            .write_all("+PONG\r\n".as_bytes())
+            .expect("Failed to PONG");
     }
 }
