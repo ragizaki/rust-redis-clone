b3c6bde78fa79f654e9df0d5dd6b502cb27d2d68
Zaki Machfj <zmachfj@uwaterloo.ca>
2024-03-20 22:23:27 -0400

    respond to PING with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(mut stream) => stream
+                .write_all("+PONG\r\n".as_bytes())
+                .expect("Failed to PONG"),
             Err(e) => {
                 println!("error: {}", e);
             }
