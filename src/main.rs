85882eb72b1cf2811fb63f1ed1bb8701edc07155
Zaki Machfj <zmachfj@uwaterloo.ca>
2024-03-26 07:59:48 -0400

    create global parser

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ async fn main() -> Result<()> {
     loop {
         let (stream, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
-            match handle_connection(stream).await {
+            let parser = Parser::new();
+            match handle_connection(stream, parser).await {
                 Ok(()) => (),
                 Err(msg) => eprintln!("Error handling connection: {}", msg),
             }
@@ -23,7 +24,7 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) -> Result<()> {
+async fn handle_connection(mut stream: TcpStream, mut parser: Parser) -> Result<()> {
     let mut buf = [0; 1024];
     loop {
         let num_bytes = stream.read(&mut buf).await?;
@@ -33,7 +34,6 @@ async fn handle_connection(mut stream: TcpStream) -> Result<()> {
         }
 
         let request = std::str::from_utf8(&buf[..num_bytes]).expect("Invalid ASCII");
-        let mut parser = Parser::new();
         let body = parser.parse(request)?;
         let payload = parser.from_array(body)?;
 
